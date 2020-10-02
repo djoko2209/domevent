8be17ae5e9402e0217693f36f06507240527ee2a
djoko2209 <dsoelastoro@gmail.com>
2020-10-02 23:09:20 +0700

    update showcase

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ const closeModal = () => {
     let modal = document.getElementById('modalMenu')
     modal.style.display = 'none'
 }
+
+//kita ambil dulu element yang diinginkan
+let showcase = document.getElementById('wrapper_showcase')
+
+
+//kita inject element nya dengan syntax HTML
+//showcase.innerHTML += `
+//<img src="https://picsum.photos/seed/19/300" />
+//`
+
+for (var i = 0; i < 36; i++) {
+    showcase.innerHTML += `
+        <img src='https://picsum.photos/seed/${100 + i}/100' alt='gambar ke ${i}'/>
+    
+    `
+
+}
